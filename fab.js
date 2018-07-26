97641025d2068af4bc5fac234465693c98655077
Marco Tavares <matr192001@hotmail.com>
2018-07-26 15:00:23 -0500

    Update fab.js

--- fab.js
+++ fab.js
@@ -1,25 +1,54 @@
-function fab(){
-  var fab_primary = $(".fab_contenedor .fab.primary"),
-      dl = $(".fab_contenedor dl"),
-      button = $(".fab_contenedor dl dt button"),
-      contenedor = $(".fab_contenedor");
-
-  fab_primary.hover(function() {
-    dl.css({"visibility": "visible","opacity": "1"});
-    button.css('transform', 'scale(1) translateY(0px) translateX(0px)');
-    contenedor.hover(function() {}, function() {
-      dl.css({"visibility": "hidden","opacity": "0"});
-      button.css('transform', 'scale(.4) translateY(40px) translateX(0px)');
-    })
-  });
-
-  fab_primary.on('click', function() {
-    if (dl.css("opacity") == 0) {
-      button.css('transform', 'scale(1) translateY(0px) translateX(0px)');
-      dl.css({"visibility": "visible","opacity": "1"});
-    }else{
-      dl.css({"visibility": "hidden","opacity": "0"});
-      button.css('transform', 'scale(.4) translateY(40px) translateX(0px)');
+function Fab(json){
+  var __element = document.querySelector(json.selector);
+  var positions = ["top-left","top-right","bottom-left","bottom-right"], countposition = 0, status = false;
+  if (!json.selector || !__element) {
+    console.error("There is no selector or you have not written one, write one in the option 'selector' in the json");
+    return false;
+  }
+  positions.forEach(function(e, i){
+    if(positions[i] != json.position){
+      countposition++;
     }
+    if (countposition == 4) {
+      __element.classList.add("default");
+    }
+  });
+  __element.classList.add("fab_contenedor");
+  __element.classList.add(json.position);
+  __element.classList.add(json.direction);
+
+  __element.insertAdjacentHTML('beforeend',
+    `<button class='fab primary ${json.button.style}'>
+      ${json.button.html}
+      <i class='${json.icon.style}'>${json.icon.html}</i>
+  	</button>`);
+
+  __element.insertAdjacentHTML('afterbegin',`<dl></dl>`);
+
+  json.buttons.forEach(function(e, i){
+    __element.querySelector('dl').insertAdjacentHTML('afterbegin',
+    `<dt>
+      <button class="fab ${e.button.style} btn${i}">
+        ${e.button.html}
+    	  <i class="${e.icon.style}">${e.icon.html}</i>
+    	</button>
+     </dt>`);
+    __element.querySelector("dl .fab.btn"+i).addEventListener("click", function(){
+      e.onClick();
+    });
+  });
+  var button = __element.querySelectorAll('dl dt button');
+
+  $(json.selector+" .fab.primary").hover(function() {
+    __element.querySelector('dl').classList.add("visible");
+    button.forEach(function(e, i) {
+      button[i].classList.add("transform");
+    });
+    $(json.selector).hover(function() {}, function() {
+      __element.querySelector('dl').classList.remove("visible");
+      button.forEach(function(e, i) {
+        button[i].classList.remove("transform");
+      });
+    })
   });
 }
